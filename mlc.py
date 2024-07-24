f8b0b9c17a0fe9f28e04a85c9875e2375b2ff9b8
hoanganhknk <hoanganhnguyen.knk@gmail.com>
2024-07-24 16:12:23 +0700

    temp

--- mlc.py
+++ mlc.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+torch.autograd.set_detect_anomaly(True)
+from SAM import *
 def _concat(xs):
     return torch.cat([x.view(-1) for x in xs])
 
 @torch.no_grad()
 def update_params(params, grads, eta, opt, args, deltaonly=False, return_s=False):
-    if isinstance(opt, torch.optim.SGD):
+    if isinstance(opt, torch.optim.SGD) or isinstance(opt, torch.optim.Adam) or isinstance(opt, SAM) :
         return update_params_sgd(params, grads, eta, opt, args, deltaonly, return_s)
     else:
         raise NotImplementedError('Non-supported main model optimizer type!')
@@ -20,12 +21,12 @@ def update_params_sgd(params, grads, eta, opt, args, deltaonly, return_s=False):
 
     if return_s:
         ss = []
-
+    if isinstance(opt, SAM):
+        opt = opt.base_optimizer
     wdecay = opt.defaults['weight_decay']
     momentum = opt.defaults['momentum']
     dampening = opt.defaults['dampening']
     nesterov = opt.defaults['nesterov']
-
     for i, param in enumerate(params):
         dparam = grads[i] + param * wdecay # s=1
         s = 1
@@ -69,8 +70,8 @@ def step_hmlc_K(main_net, main_opt, hard_loss_f,
                 meta_net, meta_opt, soft_loss_f,
                 data_s, target_s, data_g, target_g,
                 data_c, target_c, 
-                eta, args):
-
+                eta, args): 
+    
     # compute gw for updating meta_net
     logit_g = main_net(data_g)
     loss_g = hard_loss_f(logit_g, target_g)
@@ -109,26 +110,19 @@ def step_hmlc_K(main_net, main_opt, hard_loss_f,
     # 3.5 compute discount factor gw_prime * (I-LH) * gw.t() / |gw|^2
     tmp1 = [(1-Hw*dparam_s[i]) * gw_prime[i] for i in range(len(dparam_s))]
     gw_norm2 = (_concat(gw).norm())**2
-    tmp2 = [gw[i]/gw_norm2 for i in range(len(gw))]
+    tmp2 = [gw[i]/(gw_norm2 + 10e-10) for i in range(len(gw))]
     gamma = torch.dot(_concat(tmp1), _concat(tmp2))
 
     # because of dparam_s, need to scale up/down f_params_grads_prime for proxy_g/loss_g
     Lgw_prime = [ dparam_s[i] * gw_prime[i] for i in range(len(dparam_s))]     
 
     proxy_g = -torch.dot(_concat(f_param_grads), _concat(Lgw_prime))
-
-    # Back prop on alphas using SAM steps
-    def meta_net_closure():
-        meta_opt.zero_grad()
-        proxy_g.backward()
-
-    # First SAM step
-    meta_opt.first_step(zero_grad=True)
-    meta_net_closure()
     
-    # Second SAM step
-    meta_opt.second_step(zero_grad=True)
-    
+    # back prop on alphas
+    meta_opt.zero_grad()
+
+    proxy_g.backward()
+
     # accumulate discounted iterative gradient
     for i, param in enumerate(meta_net.parameters()):
         if param.grad is not None:
@@ -136,13 +130,56 @@ def step_hmlc_K(main_net, main_opt, hard_loss_f,
             args.dw_prev[i] = param.grad.clone()
 
     if (args.steps+1) % (args.gradient_steps)==0: # T steps proceeded by main_net
-        meta_opt.step(meta_net_closure)
+        meta_opt.first_step(zero_grad=True)
+        # tính proxy_g
+        logit_s, x_s_h = main_net(data_s, return_h=True)
+        pseudo_target_s = meta_net(x_s_h.detach(), target_s)
+        loss_s = soft_loss_f(logit_s, pseudo_target_s)
+
+        if data_c is not None:
+            bs1 = target_s.size(0)
+            bs2 = target_c.size(0)
+
+            logit_c = main_net(data_c)
+            loss_s2 = hard_loss_f(logit_c, target_c)
+            loss_s = (loss_s * bs1 + loss_s2 * bs2 ) / (bs1+bs2)
+
+        f_param_grads = torch.autograd.grad(loss_s, main_net.parameters(), create_graph=True)    
+
+        f_params_new, dparam_s = update_params(main_net.parameters(), f_param_grads, eta, main_opt, args, return_s=True)
+        # 2. set w as w'
+        f_param = []
+        for i, param in enumerate(main_net.parameters()):
+            f_param.append(param.data.clone())
+            param.data = f_params_new[i].data # use data only as f_params_new has graph
+        
+        # training loss Hessian approximation
+        Hw = 1 # assume to be identity 
+
+        # 3. compute d_w' L_{D}(w')
+        logit_g = main_net(data_g)
+        loss_g  = hard_loss_f(logit_g, target_g)
+        gw_prime = torch.autograd.grad(loss_g, main_net.parameters())
+
+        # 3.5 compute discount factor gw_prime * (I-LH) * gw.t() / |gw|^2
+        tmp1 = [(1-Hw*dparam_s[i]) * gw_prime[i] for i in range(len(dparam_s))]
+        gw_norm2 = (_concat(gw).norm())**2
+        tmp2 = [gw[i]/(gw_norm2 + 10e-10) for i in range(len(gw))]
+        gamma = torch.dot(_concat(tmp1), _concat(tmp2))
+
+        # because of dparam_s, need to scale up/down f_params_grads_prime for proxy_g/loss_g
+        Lgw_prime = [ dparam_s[i] * gw_prime[i] for i in range(len(dparam_s))]     
+
+        proxy_g = -torch.dot(_concat(f_param_grads), _concat(Lgw_prime))
+
+        proxy_g.backward()
+        # second step
+        meta_opt.second_step(zero_grad=True)
         args.dw_prev = [0 for param in meta_net.parameters()] # 0 to reset 
 
     # modify to w, and then do actual update main_net
     for i, param in enumerate(main_net.parameters()):
         param.data = f_param[i]
         param.grad = f_param_grads[i].data
-    main_opt.step()
-    
+    main_opt.step()    
     return loss_g, loss_s
