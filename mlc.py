107ffbdcecbf12815986d06c730fd4f73ebb0717
hoanganhknk <hoanganhnguyen.knk@gmail.com>
2024-08-22 10:45:22 +0700

    third change

--- mlc.py
+++ mlc.py
@@ -139,9 +139,18 @@ def step_hmlc_K(main_net, main_opt, hard_loss_f,
         param.grad = f_param_grads[i].data
     if isinstance(main_opt, SAM):
         main_opt.first_step(zero_grad=True)
-        logit_g = main_net(data_g)
-        loss_g = hard_loss_f(logit_g, target_g) 
-        f_param_grads = torch.autograd.grad(loss_g, main_net.parameters(), create_graph=True)    
+        logit_s, x_s_h = main_net(data_s, return_h=True)
+        pseudo_target_s = meta_net(x_s_h.detach(), target_s)
+        loss_s = soft_loss_f(logit_s, pseudo_target_s)
+
+        if data_c is not None:
+            bs1 = target_s.size(0)
+            bs2 = target_c.size(0)
+
+        logit_c = main_net(data_c)
+        loss_s2 = hard_loss_f(logit_c, target_c)
+        loss_s = (loss_s * bs1 + loss_s2 * bs2 ) / (bs1+bs2)
+        f_param_grads = torch.autograd.grad(loss_s, main_net.parameters(), create_graph=True)    
 
         f_params_new, dparam_s = update_params(main_net.parameters(), f_param_grads, eta, main_opt, args, return_s=True)
         f_param = []
